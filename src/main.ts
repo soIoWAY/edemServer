78c91b4b01e7cf6aca39b19d107b2b4d70dff655
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-03-25 13:41:58 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-
-const cookieParserMiddleware: (req: any, res: any, next: any) => void =
-  cookieParser;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +15,7 @@ async function bootstrap() {
     origin: true,
     credentials: true,
   });
-  app.use(cookieParserMiddleware);
+  app.use(cookieParser());
   await app.listen(process.env.PORT || 5555);
 }
 bootstrap();
